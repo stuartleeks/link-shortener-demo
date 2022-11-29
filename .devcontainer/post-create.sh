3e1736364255ab3cab8b7ffe94b92e6743e9085a
Stuart Leeks <stuartle@microsoft.com>
2022-11-29 06:44:14 +0000

    fixup - swa from source

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,11 @@
 set -e
 
 ## Install the Statis Web Apps CLI
-npm install -g @azure/static-web-apps-cli
+# npm install -g @azure/static-web-apps-cli
+# Due to an issue with deployment in 1.0.3, we install from source (until 1.0.4 is released)
+git clone https://github.com/azure/static-web-apps-cli /tmp/swa
+(cd /tmp/swa && git checkout 2cd0e980ea4bf604df10fd5d63149b20b292e916 && npm install && npm link)
+echo "alias swa=$(which swa)" >> ~/.bashrc
 
 ## Install the Azure Functions Core Tools
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
